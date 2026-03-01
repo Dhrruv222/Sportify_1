eaf0591be649762772106fdde5e7472937b3fc63
Gabo8912 <gavo0031@outlook.com>
2026-03-01 14:08:01 +0100

    Google auth feuature

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+require('./config/passport');
 
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
 
 //Auth route
 const authRoutes = require('./modules/auth/auth.routes');
@@ -13,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(passport.initialize());
 
 // Routes API
 app.use('/api/v1/auth', authRoutes);
--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -1,4 +1,5 @@
 const authService = require('./auth.service');
+const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
     try {
@@ -47,4 +48,43 @@ const loginUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser, loginUser };
+const googleOAuthCallback = async (req, res) => {
+    try {
+    const user = req.user;
+
+    const accessToken = jwt.sign(
+        { userId: user.id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: '15m' }
+    );
+
+    const refreshToken = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_REFRESH_SECRET,
+        { expiresIn: '30d' }
+    );
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+      maxAge: 30 * 24 * 60 * 60 * 1000
+    });
+    // Since Google Login usually happens in the browser,
+    //ideally, redirect to the frontend with the token (Dev 2 will receive it)
+    //For now, we'll return a JSON so can be test it.
+    res.status(200).json({
+        status: "success",
+        message: "Successful Google Login",
+        data: {
+        user: { id: user.id, email: user.email, role: user.role },
+        accessToken
+        }
+    });
+
+    } catch (error) {
+    res.status(500).json({ status: "error", message: "Authentication error with Google" });
+    }
+};
+
+module.exports = { registerUser, loginUser, googleOAuthCallback };
